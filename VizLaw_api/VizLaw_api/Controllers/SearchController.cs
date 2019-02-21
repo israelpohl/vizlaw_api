d93a3fbce4fa2439401516cf5347779737036107
israel <israel.pohl@estiem.org>
2019-02-21 11:27:41 +0100

    search controller

--- VizLaw_api/VizLaw_api/Controllers/SearchController.cs
+++ VizLaw_api/VizLaw_api/Controllers/SearchController.cs
@@ -10,21 +10,24 @@ using VizLaw_api.DataAccess;
 namespace VizLaw_api.Controllers
 {
     [EnableCors(origins: "*", headers: "*", methods: "*")]
-    public class ValuesController : ApiController
+    public class SearchController : ApiController
     {
         // GET api/values
         
-        public IEnumerable<Citation> Get()
-        {
-            if(CitationNetwork.Citations == null || CitationNetwork.Citations.Count() == 0)
-                CitationNetwork.LoadCitationNetwork();
-            
-            //int count = CitationNetwork.Citations.Where(ct =>ct.from_type != "Law" && ct.to_type != "Law").Count();
-
-            return CitationNetwork.Citations.Where(c=>c.to_type != "Law").Take(20);
-        }
+       
 
         // GET api/values/5
+        public nodeData Get(string text)
+        {
+            if (CitationNetwork.Citations == null || CitationNetwork.Citations.Count() == 0)
+                CitationNetwork.LoadCitationNetwork();
+
+            //int count = CitationNetwork.Citations.Where(ct =>ct.from_type != "Law" && ct.to_type != "Law").Count();
+
+            //return CitationNetwork.Citations.Where(c => c.to_id==id.ToString());
+            return CitationNetwork.getNodeData(text);
+        }
+
         public nodeData Get(int id)
         {
             if (CitationNetwork.Citations == null || CitationNetwork.Citations.Count() == 0)
@@ -36,30 +39,6 @@ namespace VizLaw_api.Controllers
             return CitationNetwork.getNodeData(id.ToString());
         }
 
-        public nodeData Search(string text)
-        {
-            if (CitationNetwork.Citations == null || CitationNetwork.Citations.Count() == 0)
-                CitationNetwork.LoadCitationNetwork();
 
-            //int count = CitationNetwork.Citations.Where(ct =>ct.from_type != "Law" && ct.to_type != "Law").Count();
-
-            //return CitationNetwork.Citations.Where(c => c.to_id==id.ToString());
-            return CitationNetwork.getNodeData(text);
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
